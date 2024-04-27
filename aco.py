dcf3d7fd054b53b68697d527b26f76cd3dab380c
Jerome Siljan <JeromeSiljan@gmail.com>
2024-04-27 11:56:35 -0500

    feat(aco.py): Switched from df to np arrays

--- aco.py
+++ aco.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import numpy as np
+import sys
 
 PHEREMONE_INITIAL_VALUE = 0.0
 Q = 1
+
+node_dict = {
+    0: "A",
+    1: "B",
+    2: "C",
+    3: "D",
+    4: "E",
+    5: "F",
+    6: "G",
+    7: "H",
+}
 
 
 class Ant:
@@ -13,8 +25,13 @@ class Ant:
 
     def announce(self):
         print(
-            f"ant {self.index} starting at point {self.starting_point} has visited {self.previously_visited}"
+            f"ant {self.index} starting at point {node_dict[self.starting_point]} has visited {self.previously_visited}"
         )
+
+
+def print_arr(arr):
+    fmt = ["%f" for _ in range(arr.shape[1])]
+    np.savetxt(sys.stdout, arr, fmt=fmt, delimiter="\t")
 
 
 def initialize():
@@ -32,26 +49,30 @@ def initialize():
             )
 
     # Convert distances to DataFrame
-    distances_df = pd.DataFrame(distances, index=df.index, columns=df.index)
-    pheremones_df = pd.DataFrame(pheremones, index=df.index, columns=df.index)
 
-    return (df, distances_df, pheremones_df)
+    # distances_df = pd.DataFrame(distances, index=df.index, columns=df.index)
+    # pheremones_df = pd.DataFrame(pheremones, index=df.index, columns=df.index)
+
+    return (df, distances, pheremones)
 
 
-df, distances_df, pheremones_df = initialize()
+df, distances, pheremones = initialize()
 
-num_ants = len(distances_df)
-num_nodes = len(distances_df)
+num_ants = len(df)
+num_nodes = len(df)
 
 ants = []
 
 # Construct ant solutions
 for ant in range(num_ants):
-    ants.append(Ant(ant, distances_df.index[ant]))
+    starting_node = ant
+    ants.append(Ant(ant, starting_node))
     current_ant = ants[ant]
     current_ant.announce()
     for node in range(num_nodes):
         if node not in current_ant.previously_visited:
-            pheremones_df.iat[current_ant.index, node] += (
-                Q / distances_df.iat[current_ant.index, node]
+            pheremones[current_ant.index][node] += (
+                Q / distances[current_ant.index][node]
             )
+
+print_arr(pheremones)
