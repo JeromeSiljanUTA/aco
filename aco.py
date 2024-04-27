b7f648dca86d926f9bd3a82ae90f523ae2158c05
Jerome Siljan <JeromeSiljan@gmail.com>
2024-04-27 15:14:37 -0500

    feat(aco.py): working through constructing ant solutions

--- aco.py
+++ aco.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-import sys
+import matplotlib.pyplot as plt
 
-PHEREMONE_INITIAL_VALUE = 0.0
+DEBUG = 0
+PHEROMONE_INITIAL_VALUE = 0.1
 EVAPORATION_CONSTANT = 0.5
 Q = 1
 
@@ -21,10 +22,11 @@ node_dict = {
 
 
 class Ant:
-    def __init__(self, index, starting_point):
+    def __init__(self, index):
+        self.index = index
         self.current_node = index
-        self.starting_point = starting_point
-        self.previously_visited = [starting_point]
+        self.starting_point = index
+        self.previously_visited = [self.starting_point]
 
     def announce(self):
         print(
@@ -34,8 +36,11 @@ class Ant:
     def choose(self, target_node):
         if self.current_node not in self.previously_visited:
             self.previously_visited.append(self.current_node)
+
         if target_node not in self.previously_visited:
-            print(f"ant at {ant.current_node} chooses node {node_dict[choice]}")
+            self.previously_visited.append(target_node)
+            if DEBUG:
+                print(f"ant at {ant.current_node} chooses node {node_dict[choice]}")
             self.current_node = target_node
 
 
@@ -44,7 +49,7 @@ def initialize():
 
     n = len(df)
     distances = np.zeros((n, n))
-    pheremones = np.full((n, n), PHEREMONE_INITIAL_VALUE)
+    pheromones = np.full((n, n), PHEROMONE_INITIAL_VALUE)
 
     for i in range(n):
         for j in range(n):
@@ -52,49 +57,70 @@ def initialize():
                 (df.iloc[i]["x_coord"] - df.iloc[j]["x_coord"]) ** 2
                 + (df.iloc[i]["y_coord"] - df.iloc[j]["y_coord"]) ** 2
             )
-    return (df, distances, pheremones)
+    return (df, distances, pheromones)
 
 
-df, distances, pheremones = initialize()
+def calc_desires(ant):
+    desires = np.zeros(num_nodes)
+    for node in range(num_nodes):
+        if node not in ant.previously_visited:
+            desires[node] = pheromones[ant.current_node][node] * (
+                1 / distances[ant.current_node][node]
+            )
+        else:
+            desires[node] = 0
 
+    return desires
+
+
+# Initialize
+df, distances, pheromones = initialize()
 num_ants = len(df)
 num_nodes = len(df)
-
 ants = []
 
-prev_pheremones = pheremones.copy()
+for ant in range(num_ants):
+    index = ant
+    ants.append(Ant(index))
+    current_ant = ants[ant]
 
-print(distances)
+prev_pheromones = pheromones.copy()
 
 # Construct ant solutions
-for ant in range(num_ants):
-    starting_node = ant
-    index = ant
-    ants.append(Ant(index, starting_node))
-    current_ant = ants[ant]
-    for node in range(num_nodes):
-        if node not in current_ant.previously_visited:
-            pheremones[current_ant.current_node][node] += (
-                Q / distances[current_ant.current_node][node]
-            )
+for ant in ants:
+    for i in range(num_nodes - 1):
+        desires = calc_desires(ant)
+        probs = desires / np.sum(desires)
+        outcomes = [node for node in range(num_nodes)]
+        choice = np.random.choice(outcomes, p=probs)
+        ant.choose(choice)
+    print(f"Ant {ant.index} path: {ant.previously_visited}")
 
 
-# Update pheremones
-prev_pheremones = prev_pheremones * (1 - EVAPORATION_CONSTANT)
-pheremones = pheremones + prev_pheremones
+old = """    
+    probs = desires / np.sum(desires)
+    outcomes = [node for node in range(num_nodes)]
+    choice = np.random.choice(outcomes, p=probs)
+    ant.choose(choice)
+    pheromones[current_ant.current_node][node] += (
+        Q / distances[current_ant.current_node][node]
+    )
+
+for idx, ant in enumerate(ants):
+    solution = ant.previously_visited
+    ant.previously_visited = [idx]
+    print(solution)
+
+# Update pheromones
+prev_pheromones = prev_pheromones * (1 - EVAPORATION_CONSTANT)
+pheromones = pheromones + prev_pheromones
 
 
-# Choose path
-ant = ants[0]
+plt.matshow(pheromones, cmap="viridis")
+plt.xticks(np.arange(8), ["A", "B", "C", "D", "E", "F", "G", "H"])
+plt.yticks(np.arange(8), ["A", "B", "C", "D", "E", "F", "G", "H"])
 
-desires = np.zeros(num_nodes)
-for node in range(num_nodes):
-    if node not in ant.previously_visited:
-        desires[node] = pheremones[ant.current_node][node] * (
-            1 / distances[ant.current_node][node]
-        )
 
-probs = desires / np.sum(desires)
-choice = np.random.choice([node for node in range(num_nodes)], p=probs)
-ant.choose(choice)
-ant.announce()
+plt.colorbar()
+# plt.show()
+"""
